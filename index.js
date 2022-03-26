d9a3a471e0103ed83a6572c5d092310064efaec3
melih164 <melih3724@gmail.com>
2022-03-26 13:16:19 +0100

    flexbox used, main. alt added,br removed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ document.querySelector(".submit").addEventListener("click", function (){
    document.querySelector("p").innerHTML = "You selected "+checked+" out of 5";
 
    $("p").after("<h2 class='thank'>Thank you!</h2>")
-   $("p").before("<div class='img1'><img  src='images/illustration-thank-you.svg'/></div>")
+   $("p").before("<div class='img1'><img alt='thank you' src='images/illustration-thank-you.svg'/></div>")
    $("h3, .buttons, .submit, .star").remove();
    $("h2").after("<p class='secpara'>We appreciate you taking the time to give a rating. If you ever need more support,don’t hesitate to get in touch!</p>")
    
